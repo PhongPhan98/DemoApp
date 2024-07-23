c704a2fd97754445f3971449f7da1adc0f338895
PhongPhan98 <phongpx9x@gmail.com>
2024-07-23 19:56:55 +0700

    enhance: logging to file by day

--- DemoApp.API/Program.cs
+++ DemoApp.API/Program.cs
@@ -17,6 +17,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var logger = new LoggerConfiguration()
     .WriteTo.Console()
+    .WriteTo.File("Logs/DemoApp_Log.txt", rollingInterval: RollingInterval.Day)
     .MinimumLevel.Information()
     .CreateLogger();
 
